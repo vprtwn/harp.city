afa20c3ed4fda613d4c069de06fa293f0c29ee80
Ben Guo <benzguo@gmail.com>
2020-05-11 14:28:37 -0700

    add react-draggable

--- pages/[place].tsx
+++ pages/[place].tsx
@@ -1,17 +1,50 @@
 import Head from "next/head";
+import Draggable from "react-draggable";
 import { useRouter } from "next/router";
+import React, { useEffect, useRef, useState } from "react";
 
 const PlacePage = (props: any) => {
+  const [deltaPosition, setDeltaPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
   const {
     query: { place },
   } = useRouter();
+
+  const cellRef = useRef<HTMLDivElement>(null);
+
+  const handleDrag = (e, ui) => {
+    const { x, y } = deltaPosition;
+    setDeltaPosition({
+      x: x + ui.deltaX,
+      y: y + ui.deltaY,
+    });
+  };
+
   return (
     <div>
       <base target="_blank" />
       <Head>
         <title>{place ?? "harp.city"}</title>
       </Head>
-      {place}
+      <div>
+        <Draggable
+          defaultPosition={{ x: 200, y: 200 }}
+          position={null}
+          grid={[50, 50]}
+          scale={1}
+          // onStart={handleStart}
+          onDrag={handleDrag}
+          // onStop={this.handleStop}
+        >
+          <div ref={cellRef} className="cell border border-solid border-black">
+            <div className="text-xs">{deltaPosition.x.toFixed(0)}</div>
+            <div className="text-xs">{deltaPosition.y.toFixed(0)}</div>
+          </div>
+        </Draggable>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 const IndexPage = () => (
   <div>
     <Head>
-      <title>Create Next App</title>
+      <title>harp.city</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
