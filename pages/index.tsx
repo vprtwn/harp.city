75328991e9f54324152fcd99f4d764c856dc8fe4
Ben Guo <benzguo@gmail.com>
2020-05-11 11:15:06 -0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const IndexPage = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <div className="p-4 shadow rounded bg-white">
+      <h1 className="text-purple-500 leading-normal">Next.js</h1>
+      <p className="text-gray-500">with Tailwind CSS</p>
+    </div>
+  </div>
+);
+
+export default IndexPage;
