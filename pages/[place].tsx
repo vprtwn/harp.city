0097442f94f8f5ccbba0fc1d6b6f4c430b7f2c51
Ben Guo <benzguo@gmail.com>
2020-05-16 17:20:38 -0700

    support deletion

--- pages/[place].tsx
+++ pages/[place].tsx
@@ -16,6 +16,8 @@ import Gun from "gun";
 import "gun/lib/webrtc";
 import Tone from "tone";
 import useWindowScroll from "react-use/lib/useWindowScroll";
+import useWindowSize from "react-use/lib/useWindowSize";
+import useScrolling from "react-use/lib/useScrolling";
 import { Stage, Layer, Text, Line } from "react-konva";
 import Konva from "konva";
 
@@ -34,7 +36,13 @@ let gunStore = null;
 
 const PlacePage = (props: any) => {
   const [nodes, setNodes] = useState([]);
-  const { x, y } = useWindowScroll();
+  const [selected, setSelected] = useState<number | null>(null);
+  const [selectedY, setSelectedY] = useState<number | null>(null);
+  const [dragging, setDragging] = useState<boolean>(false);
+  const scrollRef = React.useRef(null);
+  const scrolling = useScrolling(scrollRef);
+  const { x: scrollX, y: scrollY } = useWindowScroll();
+  const { width: windowW, height: windowH } = useWindowSize();
 
   const {
     query: { place },
@@ -47,13 +55,26 @@ const PlacePage = (props: any) => {
     const nodeName = `${prefix}^${place || ""}`;
     gunStore = gun.get(nodeName);
     gunStore.map().on((val, msgId) => {
-      nodes[val.i] = {
-        x: val.x,
-        y: val.y,
-      };
+      if (val) {
+        nodes[msgId] = {
+          x: val.x,
+          y: val.y,
+        };
+      } else {
+        nodes[msgId] = null;
+      }
       setNodes(JSON.parse(JSON.stringify(nodes)));
     });
   }
+
+  useEffect(() => {
+    if (selected !== null) {
+      const node = nodes[selected];
+      setSelectedY(node.y);
+    } else {
+      setSelectedY(null);
+    }
+  }, [selected, nodes]);
 
   return (
     <div>
@@ -63,9 +84,9 @@ const PlacePage = (props: any) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className="canvas border border-solid border-black">
+      <div ref={scrollRef} className="canvas border border-solid">
         {nodes.map((node, i) => {
-          return (
+          return node ? (
             <Draggable
               defaultClassName="react-draggable absolute"
               key={`draggable_${i}`}
@@ -77,38 +98,41 @@ const PlacePage = (props: any) => {
                   newNodes[index] = n;
                 });
                 newNodes[i] = {
-                  i: i,
                   x: p.x,
                   y: p.y,
                 };
                 setNodes(newNodes);
               }}
+              onStart={() => {
+                setDragging(true);
+              }}
               onStop={(e, d) => {
                 let node = nodes[i];
                 node.d = false;
                 gunStore.get(i).put(node);
+                setSelected(i);
+                setDragging(false);
               }}
             >
               <div
-                className={`cell border`}
+                className={`cell ${selected === i ? "border-2" : "border"}`}
                 style={{
                   borderColor: `${mToColor(yToM(node.y))}`,
+                  backgroundColor:
+                    selected === i ? `${mToColor(yToM(selectedY))}88` : `transparent`,
                 }}
-              >
-                {/* <div className="text-xs text-right">{node.x}</div> */}
-                {/* <div className="text-xs text-right">{i}</div> */}
-              </div>
+              ></div>
             </Draggable>
-          );
+          ) : null;
         })}
         {nodes.map((node, i) => {
-          return (
+          return node ? (
             <div>
               <div
                 className="string-label pl-1"
                 style={{
                   top: `${node.y}px`,
-                  left: `${x}px`,
+                  left: `${scrollX}px`,
                   color: `${mToColor(yToM(node.y))}`,
                 }}
               >
@@ -116,36 +140,53 @@ const PlacePage = (props: any) => {
                 <sup className="text-xxs">{midiToTuningSymbol(yToM(node.y))}</sup>
               </div>
             </div>
-          );
+          ) : null;
         })}
 
-        <button
-          hidden={nodes.length > 10}
-          className="fixed top-0 left-0 bg-black hover:bg-gray-500 text-white px-2 pb-1"
-          onClick={() => {
-            const i = nodes.length;
-            const node = {
-              i: i,
-              x: 100,
-              y: 100,
-            };
-            gunStore.get(i).put(node);
-          }}
-        >
-          new
-        </button>
+        <div className={`fixed top-0 ${dragging ? "invisible" : null} z-50`}>
+          <button
+            className="hover:bg-gray-800 text-gray-600 border rounded border-gray-600 text-lg px-5 py-2"
+            onClick={() => {
+              const i = nodes.length;
+              const node = {
+                x: 0.5 * windowW + scrollX - 0.5 * CELL_HEIGHT,
+                y: 0.5 * windowH + scrollY - 0.5 * CELL_HEIGHT,
+              };
+              gunStore.get(i).put(node);
+              setSelected(i);
+            }}
+          >
+            +□
+          </button>
+        </div>
+        <div className={`fixed top-0 right-0 ${dragging ? "invisible" : null} z-50`}>
+          <button
+            hidden={selected === null}
+            className="ml-3 hover:bg-gray-800 text-white border-2 rounded border-white text-lg px-5 py-2"
+            style={{
+              borderColor: mToColor(yToM(selectedY)),
+              color: mToColor(yToM(selectedY)),
+            }}
+            onClick={() => {
+              gunStore.get(selected).put(null);
+              setSelected(null);
+            }}
+          >
+            -□
+          </button>
+        </div>
 
         <Stage width={CANVAS_WIDTH} height={CANVAS_HEIGHT}>
           <Layer>
             {nodes.map((node, i) => {
-              return (
+              return node ? (
                 <Line
-                  points={[0, node.y + CELL_HEIGHT / 2.0, CANVAS_WIDTH, node.y + CELL_HEIGHT / 2.0]}
+                  points={[0, node.y + 0.5 * CELL_HEIGHT, CANVAS_WIDTH, node.y + 0.5 * CELL_HEIGHT]}
                   stroke={mToColor(yToM(node.y))}
                   strokeWidth={2}
                   dash={[2, xToSep(node.x)]}
                 />
-              );
+              ) : null;
             })}
           </Layer>
         </Stage>
